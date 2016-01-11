03a4836546a36a13788d4c1191d19c61fcff3406
Jonas Karlsson <jonas.karlsson@stim.se>
2016-01-11 11:47:03 +0100

    watch creates ocmod

--- watch.sh
+++ watch.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-fswatch -o upload/ | xargs -n1 -I{} cp -R upload/* ../opencart20/
+#fswatch -o upload/ | xargs -n1 -I{} cp -R upload/* ../opencart20/
+fswatch -o . | xargs -n1 -I{} zip -r ../opencart20-module.ocmod.zip *
+
