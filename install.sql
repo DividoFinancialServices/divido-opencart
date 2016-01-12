9677f4ecf0def8e29c7ec77b71daede3e65c2072
Jonas Karlsson <jonas.karlsson@stim.se>
2016-01-12 20:34:52 +0100

    add db table

---
+++ install.sql
@@ -0,0 +1,2 @@
+create table if not exists product_divido ( product_id int(11) not null, display char(7) not null, plans text, primary key(product_id)) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
