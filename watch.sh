519626d2c5e6a5c383f300c2faf8a9e8dfe849e5
Jonas Karlsson <jonas.karlsson@stim.se>
2016-01-14 12:32:10 +0100

    Add module packing to watcher

--- watch.sh
+++ watch.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
-#fswatch -o upload/ | xargs -n1 -I{} cp -R upload/* ../opencart20/
-fswatch -o . | xargs -n1 -I{} zip -r ../opencart20-module.ocmod.zip *
+fswatch -o upload/ | xargs -n1 -I{} cp -R upload/* ../opencart20/
+
+function pack_module {
+    date
+    perl -p -i -e 's/(<code>)(\d+)(<\/code>)/$1.($2+1).$3/e' install.xml 
+    zip -r ../opencart20-module.ocmod.zip *
+
+}
+
+export -f pack_module
+
+#fswatch -o install* src/ | xargs -n1 -I {} bash -c 'pack_module'
 
