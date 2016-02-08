b97173c9681aad3a4944b4228a0642a2046e56ab
Jonas Hässel <i@cjkg.me>
2016-02-08 20:38:04 +0100

    Move DB table creation to install.php to use DB_PREFIX

--- install.sql
+++ install.sql
@@ -1,2 +1,1 @@
-create table if not exists product_divido ( product_id int(11) not null, display char(7) not null, plans text, primary key(product_id)) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
